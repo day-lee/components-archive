0def523429f8f7d4ec97b7d92ba40afcec301226
Dayeon Lee <iamdayeonlee@gmail.com>
2024-10-18 11:24:27 +0100

    initial setup for meme generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import MemeGenerator from "./components/MemeGenerator";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="fixed top-0 left-0 w-full h-16 p-4 border-2">
+        <p className="font-bold"> Components playground</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <MemeGenerator />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/MemeGenerator/index.jsx
@@ -0,0 +1,79 @@
+const id = "id";
+const values = {
+  topText: "top",
+  bottomText: "bottom",
+  memeImg: "",
+};
+
+const URL = "https://api.imgflip.com/get_memes";
+
+function MemeGenerator() {
+  const onTextChange = () => {};
+  const onImgButtonClick = () => {};
+
+  return (
+    <>
+      <div className="w-full border-[1px] border-gray">
+        <header className="flex items-center bg-gradient-to-r from-[#672280] to-[#A626D3] w-[550px] h-[65px] text-white font-bold p-4">
+          <h2>Meme generator</h2>
+        </header>
+        <div className="px-[36px] pt-[28px] pb-[47px]">
+          <main>
+            <form className="flex gap-[33px] justify-between text-left text-[14px]">
+              <div className="w-[222px]">
+                <label htmlFor={id + "top-text"} className="font-medium">
+                  Top text
+                </label>
+                <input
+                  id="top-text"
+                  name="topText"
+                  className="border-2 border-[#D1D5DB] rounded-[5px] pl-[10px] py-[7px] w-full font-semibold "
+                  type="text"
+                  placeholder="Shut up"
+                  value={values.topText}
+                  onChange={onTextChange}
+                  maxLength="15"
+                />
+              </div>
+              <div className="w-[222px]">
+                <label htmlFor={id + "bottom-text"} className="font-medium">
+                  Bottom text
+                </label>
+                <input
+                  id="bottom-text"
+                  name="bottomText"
+                  className="border-2 border-[#D1D5DB] rounded-[5px] pl-[10px] py-[7px] w-full font-semibold"
+                  type="text"
+                  placeholder="And take my money"
+                  value={values.bottomText}
+                  onChange={onTextChange}
+                  maxLength="15"
+                />
+              </div>
+            </form>
+          </main>
+          <div>
+            <button
+              className="w-full bg-gradient-to-r from-[#672280] to-[#A626D3] text-white text-4 mt-4 mb-8"
+              aria-label="get a new image"
+              onClick={onImgButtonClick}
+            >
+              Get a new meme image 🖼
+            </button>
+          </div>
+          <div className="w-[477px] relative flex flex-col items-center justify-center font-impact text-shadow-custom text-white">
+            <div className="absolute top-4 text-[32px] font-impact">
+              {values.topText}
+            </div>
+            <img className="w-[477px]" src={values.memeImg} alt="meme" />
+            <div className="absolute bottom-4 text-[32px] font-impact text-shadow-custom text-white">
+              {values.bottomText}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default MemeGenerator;
